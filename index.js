779059252df44ca3b6a33c80b67aad3c8d07dc16
bcfh <crunchy.wafer.stick787@gmail.com>
2023-05-01 00:05:06 +0800

    chatgpt support continuous conversation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { lookAtNearestPlayer, moveWithNearestPlayer } = require('./tasks/followPl
 var bot = mineflayer.createBot({
   host: 'localhost', // minecraft server ip
   username: 'bc',
-  port: 53663,                // only set if you need a port that isn't 25565
+  port: 58425,                // only set if you need a port that isn't 25565
   // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // password: '12345678'        // set if you want to use password-based auth (may be unreliable)
 })
@@ -13,7 +13,7 @@ var bot = mineflayer.createBot({
 // 20s
 bot.on('physicTick', ()=>{lookAtNearestPlayer(bot)} )
 
-bot.on('spawn', ()=>{moveWithNearestPlayer(bot)} )
+bot.on('chat', ()=>{moveWithNearestPlayer(bot)} )
 
-bot.on('chat', (username, message)=>{chatWithGPT(username, message, bot)})
+bot.on('chat', (username, message)=>{chatWithGPT(username, user_input=message, bot)})
 
--- tasks/chatGPT.js
+++ tasks/chatGPT.js
@@ -5,7 +5,10 @@ module.exports = {
     chatWithGPT: chatWithGPT
 }
 
-async function chatWithGPT(username, message, bot) {
+const history = [];
+const messages = [];
+
+async function chatWithGPT(username, user_input, bot) {
     if (username === bot.username) return
 
     const configuration = new Configuration({
@@ -14,10 +17,29 @@ async function chatWithGPT(username, message, bot) {
 
     const openai = new OpenAIApi(configuration);
 
-    const completion = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: message,
-          });
 
-    bot.chat(completion.data.choices[0].text);
+    for (const [input_text, completion_text] of history) {
+        messages.push({ role: "user", content: input_text });
+        messages.push({ role: "assistant", content: completion_text });
+    }
+
+    messages.push({ role: "user", content: user_input });
+
+    try {
+        const completion = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: messages,
+        });
+        const completion_text = completion.data.choices[0].message.content;
+        console.log(completion_text);
+        history.push([user_input, completion_text]);
+        bot.chat(completion.data.choices[0].text);
+    } catch (error) {
+        if (error.response) {
+          console.log(error.response.status);
+          console.log(error.response.data);
+        } else {
+          console.log(error.message);
+        }
+      }
 }
